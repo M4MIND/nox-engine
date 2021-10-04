f3ef8607a9e2bb30e1ce788e694e038fb06fcc1c
Egor Sysoev <m4mindscape@gmail.com>
2021-10-05 00:47:48 +0500

    Test first project 'Cube Wave Animation'

--- previews/default/src/scripts/ExampleScript.ts
+++ previews/default/src/scripts/ExampleScript.ts
@@ -1,31 +1,20 @@
 import CubeScript from './CubeScripte';
-import {
-    Color,
-    GameObject,
-    Material,
-    MeshRendererComponent,
-    PrimitiveType,
-    ScriptComponent,
-    Vector3,
-} from '@gengine/engine';
+import { Color, GameObject, MeshRendererComponent, PrimitiveType, ScriptComponent, Vector3 } from '@gengine/engine';
 
 export default class ExampleScript extends ScriptComponent {
-    private material: Material | undefined;
-    private startTime: number = Date.now();
-
     public onStart() {
-        for (let x = -48; x < 48; x++) {
-            for (let z = -32; z > -96; z--) {
+        for (let x = 0; x < 64; x++) {
+            for (let z = 0; z < 64; z++) {
                 let gm = GameObject.createPrimitive(PrimitiveType.Cube);
                 gm.addComponent<CubeScript>(CubeScript);
 
                 let material = gm.getComponent<MeshRendererComponent>(MeshRendererComponent)?.material;
 
                 if (material) {
-                    material.color = new Color((x + 48) / 96, (64 - (96 - z)) / 64, 0.5);
+                    material.color = new Color(x / 64, z / 64, (x + z) / 128);
                 }
 
-                gm.transform.position = new Vector3(x, -24, z);
+                gm.transform.position = new Vector3(x - 32, -24, -z - 32);
             }
         }
     }
